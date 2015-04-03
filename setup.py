bbe0754b9ae992c6e6157a847477cafcdf728160
zsolt kormany <zsoltkormany@gmail.com>
2015-04-03 10:41:11 +0200

    add description to package

--- setup.py
+++ setup.py
@@ -19,10 +19,16 @@ cpp_module = Extension('lets_be_rational._LetsBeRational',
 setup(
     name='lets_be_rational',
     version='1.0.0',
+    description='''
+    Peter Jäckel's LetsBeRational an extremely fast and accurate method for
+    obtaining Black's implied from option prices with as little as two
+    iterations to maximum attainable precision on standard
+    (64 bit floating point) hardware for all possible inputs.
+    ''',
     url='http://vollib.org',
     download_url='git+ssh://git@github.com/vollib/lets_be_rational.git',
     maintainer='vollib',
-    maintainer_email='dev@vollib.com',
+    maintainer_email='support@quantycarlo.com',
     license="MIT",
     ext_modules=[cpp_module],
     packages=['lets_be_rational']
